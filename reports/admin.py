d1faad992fc00e74c7d9cfa2ac2add26b9073935
Christopher Groskopf <staringmonkey@gmail.com>
2014-12-22 10:54:41 -0800

    Add default queries when report is created. Closes #43.

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from django import forms
 from django.contrib import admin
 from grappelli.forms import GrappelliSortableHiddenMixin
 
@@ -7,17 +8,20 @@ import app_config
 from reports.models import Query, Project, ProjectQuery
 
 class QueryAdmin(admin.ModelAdmin):
+    """
+    Admin for the Query model.
+    """
     fields = ('name', 'slug', 'clan_yaml')
     prepopulated_fields = { 'slug': ('name',) }
 
 class ProjectQueryInline(GrappelliSortableHiddenMixin, admin.TabularInline):
+    """
+    Admin for the ProjectQuery M2M inline.
+    """
     model = ProjectQuery
     extra = 3 
     sortable_field_name = 'order'
-
-    def __init__(self, *args, **kwargs):
-        super(ProjectQueryInline, self).__init__(*args, **kwargs)
-
+    
 def run_reports(model_admin, request, queryset):
     """
     Run reports for selected projects.
@@ -32,15 +36,49 @@ def run_reports(model_admin, request, queryset):
 run_reports.short_description = 'Run reports'
 
 class ProjectAdmin(admin.ModelAdmin):
+    """
+    Admin for the Project model.
+    """
     fields = ('title', 'slug', 'property_id', 'domain', 'prefix', 'start_date')
     prepopulated_fields = { 'slug': ('title',) }
-    inlines = (ProjectQueryInline,)
 
     list_display = ('title', 'property_id', 'domain', 'prefix', 'start_date', 'view_reports')
     list_display_links = ('title',)
     list_filter = ('property_id', 'domain')
     search_fields = ('title',)
     actions = (run_reports,)
+
+    def change_view(self, *args, **kwargs):
+        """
+        Change view, with inlines.
+        """
+        self.inlines = (ProjectQueryInline,)
+
+        return super(ProjectAdmin, self).change_view(*args, **kwargs)
+
+    def add_view(self, *args, **kwargs):
+        """
+        Add view, without inlines.
+        """
+        self.inlines = ()
+
+        return super(ProjectAdmin, self).add_view(*args, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        """
+        Save the model, adding references to default queries if needed.
+        """
+        add_default_queries = not bool(obj.pk)
+
+        obj.save()
+
+        if add_default_queries:
+            for i, query_slug in enumerate(app_config.DEFAULT_QUERIES):
+                ProjectQuery.objects.create(
+                    project=obj,
+                    query=Query.objects.get(slug=query_slug),
+                    order=i
+                )
 
     def view_reports(self, model):
         url = '%s/reports/%s/' % (app_config.S3_BASE_URL, model.slug)
