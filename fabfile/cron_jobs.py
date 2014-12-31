1c0fc020b6833b21d9bb0cc0b786cbd22dd38c50
Christopher Groskopf <staringmonkey@gmail.com>
2014-12-31 08:49:28 -0800

    Add device type graphs to geckoboard.

--- fabfile/cron_jobs.py
+++ fabfile/cron_jobs.py
@@ -63,12 +63,33 @@ def run_reports(overwrite='false'):
                 [app_config.EMAIL_NOTIFY_ADDRESS]
             )
 
+GECKOBOARD_WIDGETS = {
+    'projects': [{
+        'title': '123621-8996005e-6ad7-4c99-8d71-326e14377926',
+        'sessions': '123621-96934a20-e4d1-4241-b4f2-eb194397b799',
+        'social': '77517-6d790ab0-7333-0132-9ead-22000b490a2f',
+        'devices': '123621-7004cb03-40fc-4391-8792-d84a5c020043'
+    }, {
+        'title': '77517-9da9ad80-7332-0132-9eab-22000b490a2f',
+        'sessions': '77517-d0351800-7332-0132-9eac-22000b490a2f',
+        'social': '77517-a547fda0-7333-0132-df92-22000b51936c',
+        'devices': '77517-457cf730-7338-0132-eefa-22000b5e86d6'
+    }, {
+        'title': '77517-1c6c2d80-7336-0132-9ec6-22000b490a2f',
+        'sessions': '77517-1e591bc0-7336-0132-eef8-22000b5e86d6',
+        'social': '77517-2020a130-7336-0132-7329-22000b5391df',
+        'devices': '77517-4862e280-7338-0132-df9d-22000b51936c'
+    }],
+    'sessions_leaderboard': '123621-0528aa9d-a700-43d0-ae59-f6ce5cf42984'
+}
+
 @task
 def update_geckoboard():
     top = Project.objects.all()[:3] 
 
     for i, project in enumerate(top):
         widgets = GECKOBOARD_WIDGETS['projects'][i]
+        all_time_report = project.all_time_report
 
         _geckoboard_text(
             widgets['title'],
@@ -77,18 +98,31 @@ def update_geckoboard():
 
         _geckoboard_number(
             widgets['sessions'],
-            project.all_time_report.sessions,
+            all_time_report.sessions,
             'sessions'
         )
 
         _geckoboard_number(
             widgets['social'],
-            float(project.social.total()) / project.all_time_report.sessions,
+            float(project.social.total()) / all_time_report.sessions,
             'social interactions per session'
         )
 
+        query_result = all_time_report.query_results.get(query__slug='sessions-by-device-category')
+        sessions_metric = query_result.metrics.all()[0]
+        desktop_dimension = sessions_metric.dimensions.get(name='desktop')
+        mobile_dimension = sessions_metric.dimensions.get(name='mobile')
+        tablet_dimension = sessions_metric.dimensions.get(name='tablet')
+
+        _geckoboard_rag(
+            widgets['devices'],
+            ('Mobile', '%.1f%%' % mobile_dimension.percent_of_total),
+            ('Tablet', '%.1f' % tablet_dimension.percent_of_total),
+            ('Desktop', '%.1f' % desktop_dimension.percent_of_total)
+        )
+
     top_sessions = []
-    all_time_reports = Report.objects.filter(ndays__isnull=True).order_by('-sessions')[:12]
+    all_time_reports = Report.objects.filter(ndays__isnull=True).order_by('-sessions')[:10]
 
     for report in all_time_reports:
         top_sessions.append((report.project.title, report.sessions))
@@ -155,20 +189,16 @@ def _geckoboard_leaderboard(widget_key, pairs):
 
     _geckoboard_push(widget_key, data)
 
-GECKOBOARD_WIDGETS = {
-    'projects': [{
-        'title': '123621-8996005e-6ad7-4c99-8d71-326e14377926',
-        'sessions': '123621-96934a20-e4d1-4241-b4f2-eb194397b799',
-        'social': '77517-6d790ab0-7333-0132-9ead-22000b490a2f'
-    }, {
-        'title': '77517-9da9ad80-7332-0132-9eab-22000b490a2f',
-        'sessions': '77517-d0351800-7332-0132-9eac-22000b490a2f',
-        'social': '77517-a547fda0-7333-0132-df92-22000b51936c'
-    }, {
-        'title': '77517-1c6c2d80-7336-0132-9ec6-22000b490a2f',
-        'sessions': '77517-1e591bc0-7336-0132-eef8-22000b5e86d6',
-        'social': '77517-2020a130-7336-0132-7329-22000b5391df'
-    }],
-    'sessions_leaderboard': '123621-0528aa9d-a700-43d0-ae59-f6ce5cf42984'
-}
+def _geckoboard_rag(widget_key, red_pair, orange_pair, green_pair):
+    data = {
+        'item': []
+    }
+
+    for label, value in [red_pair, orange_pair, green_pair]:
+        data['item'].append({
+            'value': value,
+            'text': label
+        })
+
+    _geckoboard_push(widget_key, data)
 
