1d9c284dfe1c50521cd4bc5272fc486601753796
Christopher Groskopf <staringmonkey@gmail.com>
2015-01-06 10:53:11 -0800

    Failsafe for 0 FB shares.

--- reports/templatetags/social.py
+++ reports/templatetags/social.py
@@ -18,6 +18,9 @@ def social_per_session(project, metric):
         return 0
 
     if metric == 'facebook_likes' or metric == 'facebook_comments':
+        if not fb_shares:
+            return 0
+
         return '%.2f' % (float(value) / fb_shares)
     else:
         return '%.2f' % (float(value) / sessions)
