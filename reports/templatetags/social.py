e23cb7d7f95066db929127bb0a0bccc482f7b1bc
Christopher Groskopf <staringmonkey@gmail.com>
2015-03-20 15:00:40 -0400

    Guard against additional data errors.

--- reports/templatetags/social.py
+++ reports/templatetags/social.py
@@ -16,8 +16,14 @@ def social_per_1000_sessions(project, metric):
 
     try:
         if metric == 'facebook_likes' or metric == 'facebook_comments':
+            if not fb_shares:
+                return 0
+
             return '%.2f' % (float(value) / fb_shares)
         else:
+            if not sessions:
+                return 0
+                
             return '%.2f' % (float(value) / (sessions / 1000))
     except ZeroDivisionError:
         return 0
